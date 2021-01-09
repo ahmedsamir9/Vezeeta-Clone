f577208380b7dad149e1066eb57419748d15fa51
abdallah1999 <abdallah.hossam32@gmail.com>
2021-01-10 01:15:00 +0200

    Login and signed up

---
+++ app/src/main/java/com/example/vezetaaclone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vezetaaclone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        finish();
+    }
+}
